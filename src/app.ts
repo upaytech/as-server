1c65ebbdf1ca32162503652d39b8613f4c9f8ce8
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-03-01 23:36:59 +0530

    feat(app): separate express and server layer

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+export default app;
