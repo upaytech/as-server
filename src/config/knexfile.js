f6ea23abba4ad72ad6e2ccc8ef3e3688a71fa250
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-03-02 07:34:39 +0530

    chore(knex): make knex work with js instead of ts

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -2,11 +2,11 @@ module.exports = {
     client: 'pg',
     version: '7.2',
     connection: {
-      host : '127.0.0.1',
-      user : 'upay',
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
       password : 'password',
-      database : 'apnasamaan',
-      port: '15432',
+      database : process.env.DB_NAME,
+      port: '5432',
     },
     pool: { min: 0, max: 7 },
 }
